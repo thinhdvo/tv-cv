06a7ce296bfee895a2a927dd019ac5663299f8a7
Thinh <thinhdvo>
2019-06-12 16:03:48 -0700

    update

--- js/main.js
+++ js/main.js
@@ -28,42 +28,6 @@ $(this).addClass("active");
 });
 
 
-//autoplay testimonial
-jQuery(document).ready(function($) {
-
-	var owl = $('.owl-carousel');
-owl.owlCarousel({
-    loop:true,
-    margin:10,
-    autoplay:true,
-    autoplayTimeout:2000,
-    autoplayHoverPause:true,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1,
-            nav:true
-        },
-        600:{
-            items:2,
-            nav:false
-        },
-        1000:{
-            items:6,
-            nav:true,
-            loop:false
-        }
-    }
-});
-$('.play').on('click',function(){
-    owl.trigger('play.owl.autoplay',[1000])
-})
-$('.stop').on('click',function(){
-    owl.trigger('stop.owl.autoplay')
-})
-	
-});
-
 //navbar
 $(document).ready(function() {
 
